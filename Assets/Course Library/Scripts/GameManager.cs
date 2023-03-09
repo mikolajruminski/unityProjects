638b49df8222087722618d2eedbab51c6d67d021
mikolajruminski <mikolaj.ruminski@gmail.com>
2023-03-09 12:01:18 +0100

    Bonus features 5, 2. Lives UI (done)

---
+++ Assets/Course Library/Scripts/GameManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{    
+    public List <GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public GameObject titleScreen;
+    public GameObject [] lives;
+    public GameObject livesObject;
+    public Button restartButton;
+
+    public int livesNumber;
+    
+    public TextMeshProUGUI gameOverText;
+    public int score;
+    private float spawnRate = 1.0f;
+
+    public bool isGameActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+         livesNumber = 4;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator spawnEnemies () {
+        
+       while (isGameActive) {
+
+        yield return new WaitForSeconds(spawnRate);
+        int randomEnemy = Random.Range(0, targets.Count);
+        Instantiate(targets[randomEnemy]);
+       }
+    }
+
+    public void updateScore(int addScore) {
+        score += addScore;
+        scoreText.text = "Score: " + score;
+    }
+    
+    public void gameOver() {
+        gameOverText.gameObject.SetActive(true);  
+        isGameActive = false;
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void GameResatart () {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+   public void StartGame(int difficulty) {
+        isGameActive = true;
+        livesObject.gameObject.SetActive(true);
+        score = 0;
+        spawnRate /= difficulty;
+        StartCoroutine(spawnEnemies());
+        updateScore(0);
+        titleScreen.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Course Library/Scripts/Target.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRB;
+
+    private GameManager gameManager;
+
+    private float maxTorque= 10;
+    private float minSpeed = 14;
+    private float maxSpeed = 18;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+
+    private int score = 5;
+    private int minusLives = 1;
+
+    private int noLives = 2;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRB = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        targetRB.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRB.AddTorque(RandomTorque(), RandomTorque(),RandomTorque(), ForceMode.Impulse);
+
+        transform.position = randomPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3 RandomForce () {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque () {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 randomPos () {
+     return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (gameManager.isGameActive) {
+        Destroy(gameObject);
+        gameManager.updateScore(-score);
+        }
+
+        if (!gameObject.CompareTag("Bad") && gameManager.isGameActive) {
+            gameManager.livesNumber -= minusLives;
+            gameManager.lives[gameManager.livesNumber].gameObject.SetActive(false);
+     }
+
+     if (gameManager.livesNumber < noLives) {
+            gameManager.gameOver();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive){
+        Destroy(gameObject);
+        gameManager.updateScore(pointValue);
+        Instantiate(explosionParticle, transform.position, transform.rotation);
+        }
+    }
+    
+}
